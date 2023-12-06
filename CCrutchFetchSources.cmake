9c9bf00ef50d6bb710df08298f7e21d4387d393c
Igor Koznov <igor.koznov@wartsila.com>
2023-12-06 23:51:19 +0100

    better git origin scanner

--- CCrutchFetchSources.cmake
+++ CCrutchFetchSources.cmake
@@ -15,16 +15,43 @@ include(FetchContent)
 
 set(CCRUTCH_EXTERNAL_DIR "${CMAKE_SOURCE_DIR}/external" CACHE PATH "")
 
-if(DEFINED ENV{CI_JOB_TOKEN})
-    # if fails, disable GitLab project setting:
-    # Settings -> CI/CD -> Token Access -> Limit access to this project
-    set(CCRUTCH_GIT_URL_BASE "https://gitlab-ci-token:$ENV{CI_JOB_TOKEN}@gitlab.com/" CACHE STRING "")
-elseif(DEFINED ENV{GITHUB_TOKEN})
-    set(CCRUTCH_GIT_URL_BASE "https://x-access-token:$ENV{GITHUB_TOKEN}@github.com/" CACHE STRING "")
-    #set(GIT_URL_BASE "https://x-access-token:$ENV{GITHUB_TOKEN}@github.com/iKoznov-GitLab-Mirror")
-else()
-    set(CCRUTCH_GIT_URL_BASE "git@gitlab.com:" CACHE STRING "")
-endif()
+find_package(Git REQUIRED)
+
+block()
+    execute_process(
+            COMMAND "${GIT_EXECUTABLE}" config --get remote.origin.url
+            OUTPUT_VARIABLE origin
+            COMMAND_ERROR_IS_FATAL ANY
+    )
+
+    string(FIND "${origin}" "https://gitlab.com/" not_gitlab)
+    if(NOT not_gitlab)
+        if(DEFINED ENV{CI_JOB_TOKEN})
+            # if fails, disable GitLab project setting:
+            # Settings -> CI/CD -> Token Access -> Limit access to this project
+            set(CCRUTCH_GIT_URL_BASE "https://gitlab-ci-token:$ENV{CI_JOB_TOKEN}@gitlab.com/")
+        else()
+            set(CCRUTCH_GIT_URL_BASE "https://gitlab.com/")
+        endif()
+    endif()
+
+    string(FIND "${origin}" "https://github.com/" not_github)
+    if(NOT not_github)
+        if(DEFINED ENV{GITHUB_TOKEN})
+            set(CCRUTCH_GIT_URL_BASE "https://x-access-token:$ENV{GITHUB_TOKEN}@github.com/")
+            #set(GIT_URL_BASE "https://x-access-token:$ENV{GITHUB_TOKEN}@github.com/iKoznov-GitLab-Mirror")
+        else()
+            set(CCRUTCH_GIT_URL_BASE "https://github.com/")
+        endif()
+    endif()
+
+    if(NOT CCRUTCH_GIT_URL_BASE)
+        #set(CCRUTCH_GIT_URL_BASE "git@gitlab.com:" CACHE STRING "")
+        message(FATAL_ERROR "please provide git remote origin url")
+    endif()
+
+    set(CCRUTCH_GIT_URL_BASE "${CCRUTCH_GIT_URL_BASE}" CACHE STRING "")
+endblock()
 
 
 function(ccrutch_fetch_sources name)
